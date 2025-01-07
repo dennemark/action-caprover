29bfa644a7fbc7d5a2379c6db8035a01aa6303a5
dennemark <martin@formfollowsyou.com>
2025-01-07 13:49:47 +0100

    fix if else fi

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,12 @@ then
     caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD -a $INPUT_APP -i $INPUT_IMAGE
   else
     caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD --appToken $INPUT_APPTOKEN -a $INPUT_APP -i $INPUT_IMAGE
+  fi
 else
   if [ x$INPUT_APPTOKEN != x ]
   then
     caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD -a $INPUT_APP -b $INPUT_BRANCH
   else
     caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD --appToken $INPUT_APPTOKEN -a $INPUT_APP -b $INPUT_BRANCH
+  fi
 fi
